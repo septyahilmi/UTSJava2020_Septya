299c6598ba555b54e6f58ba45aa1c9e951386fa2
Septya <61260357+septyahilmi@users.noreply.github.com>
2020-09-09 12:58:23 +0700

    Update UTS.java

--- UTS.java
+++ UTS.java
@@ -5,8 +5,8 @@ public class UTS {
 		String nama = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
 		
 		String panggil = ""+ nama.charAt(18)+nama.charAt(4)+nama.charAt(15)+nama.charAt(19)+
-		nama.charAt(24)+nama.charAt(0)+nama.charAt(27)+nama.charAt(7)+
-		nama.charAt(8)+nama.charAt(11)+nama.charAt(12)+nama.charAt(8);
+				 nama.charAt(24)+nama.charAt(0)+nama.charAt(27)+nama.charAt(7)+
+				 nama.charAt(8)+nama.charAt(11)+nama.charAt(12)+nama.charAt(8);
 		
 		System.out.println(panggil);
 	}
