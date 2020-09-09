88f06b7827b68d59b9a62a59ab5cdbea4ed7f88d
Septya <61260357+septyahilmi@users.noreply.github.com>
2020-09-09 12:57:26 +0700

    Add files via upload

---
+++ UTS.java
@@ -0,0 +1,15 @@
+package SeptyaHilmidayanti_XIIRPLB_UTS2020;
+
+public class UTS {
+	public static void main(String[]args) {
+		String nama = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+		
+		String panggil = ""+ nama.charAt(18)+nama.charAt(4)+nama.charAt(15)+nama.charAt(19)+
+		nama.charAt(24)+nama.charAt(0)+nama.charAt(27)+nama.charAt(7)+
+		nama.charAt(8)+nama.charAt(11)+nama.charAt(12)+nama.charAt(8);
+		
+		System.out.println(panggil);
+	}
+}
+
+
